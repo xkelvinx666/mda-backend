fec0f6850038f3df8b6bbfaee83a5424a2acfa14
captain.cai <captain.cai@icloud.com>
2019-03-15 00:24:44 +0800

    :art: 增加migrate

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,16 @@ import {ApplicationConfig} from '@loopback/core';
 export {ModelDrivenArchitecture};
 
 export async function main(options: ApplicationConfig = {}) {
+  options = {
+    ...options,
+    rest: {
+      basePath: '/api',
+      port: 3001,
+    },
+  };
   const app = new ModelDrivenArchitecture(options);
   await app.boot();
+  await app.migrateSchema();
   await app.start();
 
   const url = app.restServer.url;
